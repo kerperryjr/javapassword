9e0200f65b59bdc9838882d02f42bfe40d3bb6f2
kerperryjr <keriperryjr@gmail.com>
2023-04-06 17:59:50 -0500

    added all video code

--- Develop/script.js
+++ Develop/script.js
@@ -3,27 +3,45 @@ let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-let lowerAlphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-let upperAlphabet = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-let numbers = [0,1,2,3,4,5,6,7,8,9]
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let builtArray = [...upperAlphabet, ...numbers]
 
 function generatePassword(){
 
-if(confirm("do you want your password to contain lowercase letters"))
+  if (confirm("Do you want your password to contain lowercase letters?")) 
+  {
+    builtArray = [...builtArray, ...lowerAlphabet];
 
+  }
+  
+  let password = "";
 
+  let passwordLength =prompt("how long do you want the password?")
+  for (let i = 0; i < passwordLength; i++) {
+  
+  
+    password += "";
 
-return "password example"  
+  }
+ 
+  
+  return password;
 }
 
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-
+  let generateLetterIndex = Math.floor(Math.random() *26);
   passwordText.value = password;
+  builtArray.lowerAlphabet[generateLetterIndex]
 
+
+  return ;
 }
 
 // Add event listener to generate button
@@ -37,7 +55,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-
-Math.floor(math.random()*26);
