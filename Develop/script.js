9b201a3554dc12c817e3aefb57ff31fc983a009d
kerperryjr <keriperryjr@gmail.com>
2023-04-06 09:31:18 -0500

    added code from video

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,26 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+let lowerAlphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+
+let upperAlphabet = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
+
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+
+function generatePassword(){
+
+if(confirm("do you want your password to contain lowercase letters"))
+
+
+
+return "password example"  
+}
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +28,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+Math.floor(math.random()*26);
